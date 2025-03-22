ba3629b309466f5cc7e794e91233bdaefbb92d6f
xodls <pider6@naver.com>
2025-03-23 02:13:06 +0900

    FEATURE: 일반 User 생성

--- customuser/userapp/models.py
+++ customuser/userapp/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class CustomUSer(models.Model):
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, nickname, name, password=None):
+        if not email:
+            raise ValueError('이메일을 입력해야 합니다')
+        if not nickname:
+            raise ValueError('닉네임 입력')
+        if not name:
+            raise ValueError('이름 입력')
+        user = self.model(
+            email = self.normalize_email(email),
+            nickname = nickname,
+            name = name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
     
